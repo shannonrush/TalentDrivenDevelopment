df8e984e6489b72a2827b17cb6fd5f54a218f63d
Shannon Rush <shannonmrush@gmail.com>
2012-06-07 19:26:51 -0600

    removing cert initializer

--- config/initializers/root_certificates.rb
+++
@@ -1,9 +0,0 @@
-require 'net/https'
-class Net::HTTP
-  alias orig_initialize initialize
-  def initialize(*args,&blk)
-    orig_initialize(*args,&blk)
-    self.ca_path = '/etc/ssl/certs' if File.exists?('/etc/ssl/certs') # Ubuntu
-    #self.ca_file = '/opt/local/share/curl/curl-ca-bundle.crt' if File.exists?('/opt/local/share/curl/curl-ca-bundle.crt') # Mac OS X
-  end
-end
