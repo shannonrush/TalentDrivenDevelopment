3864b14c950951845e0332d14ed35e84ec3347af
Shannon Rush <shannonmrush@gmail.com>
2012-07-10 09:21:24 -0600

    changing update attribute to update column to properly manage callbacks

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,7 +3,8 @@ class Request < ActiveRecord::Base
   belongs_to :agent
 
   after_create :send_new_request_emails,:make_pending
-  after_update :update_associations,:send_update_request_emails,:remove_pending
+  after_update :update_associations,:send_update_request_emails
+  after_update :remove_pending
   attr_accessible :accepted, :agent_id, :message, :pending, :talent_id
 
   validates_inclusion_of :accepted, :on => :update, :in => [true,false]
@@ -14,7 +15,7 @@ class Request < ActiveRecord::Base
   end
 
   def make_pending
-    self.update_attribute(:pending,true)
+    self.update_column(:pending,true)
   end
 
   def update_associations
@@ -25,7 +26,7 @@ class Request < ActiveRecord::Base
   end
 
   def remove_pending
-    self.update_attribute(:pending,false)
+    self.update_column(:pending,false)
   end
 
 
